d1c963553a9dab9fc5be2fe4828c279a14c485eb
bunyod <bunyodbaxtiyarov1@gmail.com>
2023-11-03 13:11:29 +0500

    Test class

---
+++ src/Lecture2/Test.java
@@ -0,0 +1,10 @@
+package Lecture2;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
